d3e34ee8ba65176e7b341b12abfa20be4a5a94f6
Y-Fujikawa <fujikawa0@gmail.com>
2023-12-05 15:24:55 +0900

    Hello Lambdaまで写経
    https://cdkworkshop.com/ja/20-typescript/30-hello-cdk/200-lambda.html

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,27 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // サンプルコードの削除
+    // https://cdkworkshop.com/ja/20-typescript/30-hello-cdk/100-cleanup.html
+    // const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
+    //   visibilityTimeout: Duration.seconds(300)
+    // });
+
+    // const topic = new sns.Topic(this, 'CdkWorkshopTopic');
+
+    // topic.addSubscription(new subs.SqsSubscription(queue));
+
+    // https://cdkworkshop.com/ja/20-typescript/30-hello-cdk/200-lambda.html
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X, // execution environment
+      code: lambda.Code.fromAsset('lambda'), // code loaded from "lambda" directory
+      handler: 'hello.handler' // file is "hello", function is "handler"
     });
-
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
